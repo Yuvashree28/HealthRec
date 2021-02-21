6093524f3d51fba3b000e56fb76d0b7d116df87a
Yuvashree28 <yuvashree2827@gmail.com>
2021-02-21 08:36:23 +0530

    updated

--- HealthRecFlutterMobileApllication/android/app/src/main/kotlin/com/example/healthrec/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.healthrec
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- HealthRecFlutterWebApplication/android/app/src/main/kotlin/com/example/healthrecweb/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.healthrecweb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
